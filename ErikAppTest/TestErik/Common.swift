057d7045e35bd52ce44560480099a49604d968aa
Samuele Zolfanelli <samdazel@gmail.com>
2024-01-16 15:56:06 +0100

    Format code and remove unused globals and tests

--- ErikAppTest/TestErik/Common.swift
+++ ErikAppTest/TestErik/Common.swift
@@ -9,10 +9,3 @@
 import Foundation
 
 let googleURL = URL(string: "https://www.google.com")!
-
-
-#if os(OSX)
-let googleFormSelector = "f"
-#elseif os(iOS)
-let googleFormSelector = "gs"
-#endif
--- ErikAppTest/TestErik/ViewController.swift
+++ ErikAppTest/TestErik/ViewController.swift
@@ -13,35 +13,34 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet var redView: UIView!
-
+    
     var browser: Erik!
     var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Erik"
-
-
+        
+        
         let frame = UIScreen.main.bounds
         webView = WKWebView(frame: frame, configuration:  WKWebViewConfiguration())
         webView.allowsBackForwardNavigationGestures = true
-       redView.addSubview(webView)
-
+        redView.addSubview(webView)
+        
         webView.navigationDelegate = self
-
+        
         browser = Erik(webView: webView)
         browser.visit(url: googleURL) { object, error in
             if let e = error {
-
+                
                 print(String(describing: e))
             } else if let doc = object {
                 // HTML Inspection
                 print(String(describing: doc))
             }
-          
+            
         }
-
-
+        
     }
-
+    
 }
 
